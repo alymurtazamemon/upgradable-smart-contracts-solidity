eb7fb38317010950da9efe5aa5874c62c2d00eb1
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-21 21:07:45 +0500

    install dotenv dependency.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
 import "@openzeppelin/hardhat-upgrades";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
