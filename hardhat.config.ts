3905e197ac4f0a5ed22daeec0506d51f2f48c5b3
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-21 21:03:43 +0500

    Add configurations to hardhat.config.ts file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,29 @@ import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            chainId: 11155111,
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY!],
+            saveDeployments: true,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
