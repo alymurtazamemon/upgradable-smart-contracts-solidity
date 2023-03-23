911c8e0563110b3f2f09b42311b5ab629b10ee46
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-23 14:39:20 +0500

    Update test

--- contracts/TransparentProxyPattern/ImplementationV2.sol
+++ contracts/TransparentProxyPattern/ImplementationV2.sol
@@ -2,18 +2,13 @@
 
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract ImplementationV2 is Initializable, OwnableUpgradeable {
+contract ImplementationV2 is OwnableUpgradeable {
     uint256 private _value;
 
     // * Emitted when the stored value changes
     event ValueChanged(uint256 value);
-
-    function initialize() public initializer {
-        _value = 0;
-    }
 
     // * Stores a new value in the contract
     function store(uint256 value) public {
--- test/proxy.test.ts
+++ test/proxy.test.ts
@@ -20,19 +20,32 @@ import { expect } from "chai";
               proxy = await upgrades.deployProxy(implementationV1);
           });
 
-          it("should read the value and version no from implementation V1.", async () => {
-              const initialValue = await proxy.retrieve();
-              version = await proxy.version();
-              expect(initialValue).to.be.equal(0);
-              expect(version).to.be.equal(1);
+          describe("store", () => {
+              it("should store the value from implementation V1.", async () => {
+                  const tx: ContractTransaction = await proxy.store(40);
+                  await tx.wait(1);
+
+                  const value = await proxy.retrieve();
+                  expect(value).to.be.equal(40);
+              });
+
+              it("should emit an ValueChanged event.", async () => {
+                  expect(proxy.store(40)).to.emit(proxy, "ValueChanged");
+              });
           });
 
-          it("should store the value from implementation V1.", async () => {
-              const tx: ContractTransaction = await proxy.store(40);
-              await tx.wait(1);
+          describe("retrieve", () => {
+              it("should read the value from implementation V1.", async () => {
+                  const initialValue = await proxy.retrieve();
+                  expect(initialValue).to.be.equal(0);
+              });
+          });
 
-              const value = await proxy.retrieve();
-              expect(value).to.be.equal(40);
+          describe("version", () => {
+              it("should read the version no from implementation V1.", async () => {
+                  version = await proxy.version();
+                  expect(version).to.be.equal(1);
+              });
           });
 
           describe("After Updating to Implementation V2.", () => {
@@ -50,21 +63,53 @@ import { expect } from "chai";
                   );
               });
 
-              it("should read the value and version no from implementation V2.", async () => {
-                  version = await upgradedProxy.version();
-                  expect(version).to.be.equal(2);
-
-                  const value = await upgradedProxy.retrieve();
-                  expect(value).to.be.equal(40);
+              describe("retrieve", () => {
+                  it("should read the value from implementation V2.", async () => {
+                      const value = await upgradedProxy.retrieve();
+                      expect(value).to.be.equal(40);
+                  });
               });
 
-              it("should increment the value from implementation V2.", async () => {
-                  const tx: ContractTransaction =
-                      await upgradedProxy.increment();
-                  await tx.wait(1);
+              describe("version", () => {
+                  it("should read the value and version no from implementation V2.", async () => {
+                      version = await upgradedProxy.version();
+                      expect(version).to.be.equal(2);
+                  });
+              });
 
-                  const value = await upgradedProxy.retrieve();
-                  expect(value).to.be.equal(41);
+              describe("increment", () => {
+                  it("should increment the value from implementation V2 and emit an ValueChanged event.", async () => {
+                      const tx: ContractTransaction =
+                          await upgradedProxy.increment();
+                      await tx.wait(1);
+
+                      const value = await upgradedProxy.retrieve();
+                      expect(value).to.be.equal(41);
+
+                      expect(upgradedProxy.increment()).to.emit(
+                          upgradedProxy,
+                          "ValueChanged"
+                      );
+                  });
+              });
+
+              describe("store", () => {
+                  it("should store the value from implementation V2.", async () => {
+                      const tx: ContractTransaction = await upgradedProxy.store(
+                          786
+                      );
+                      await tx.wait(1);
+
+                      const value = await upgradedProxy.retrieve();
+                      expect(value).to.be.equal(786);
+                  });
+
+                  it("should emit an ValueChanged event.", async () => {
+                      expect(upgradedProxy.store(786)).to.emit(
+                          upgradedProxy,
+                          "ValueChanged"
+                      );
+                  });
               });
           });
       });
